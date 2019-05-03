e55aa5c5ecfb94d8003ad5e5ef61155893791535
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-03 13:27:11 +0100

    Pass decrypting unit test

--- pkg/files/secret.go
+++ pkg/files/secret.go
@@ -3,15 +3,17 @@ package files
 import (
 	"crypto/aes"
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"strings"
 )
 
-const magicChecksum = "::::MAGIC::::"
+const (
+	magicChecksum = "::::MAGIC::::"
+)
 
-func Decrypt(masterKeyPath string, hudonsSecretPath string) string {
+func Decrypt(masterKeyPath string, hudonsSecretPath string) ([]byte, error) {
 	masterKey, err := ioutil.ReadFile(masterKeyPath)
 	check(err)
 	secret, err := ioutil.ReadFile(hudonsSecretPath)
@@ -20,16 +22,17 @@ func Decrypt(masterKeyPath string, hudonsSecretPath string) string {
 	hashedMasterKey := hashMasterKey(masterKey)
 
 	encryptedSecret := decryptAes128Ecb(secret, hashedMasterKey)
-	if !strings.Contains(string(encryptedSecret), "::::MAxGIC::::") {
+	if strings.Contains(string(encryptedSecret), magicChecksum) {
+		return encryptedSecret, nil
+	} else {
 		msg := fmt.Sprintf(
 			"Error. Decrypted secret does not contain expected checksum.\n"+
 				"Expected checksum:\n\t%s\n"+
 				"Decrypted secret:\n\t%q",
 			magicChecksum,
 			encryptedSecret)
-		log.Panic(msg)
+		return nil, errors.New(msg)
 	}
-	return ""
 }
 
 /*
@@ -44,12 +47,12 @@ func hashMasterKey(masterKey []byte) []byte {
 /*
    ECB mode is deprecated and not included in golang crypto library.
 */
-func decryptAes128Ecb(ecnryptedData []byte, key []byte) []byte {
-	cipher, _ := aes.NewCipher([]byte(key))
-	decrypted := make([]byte, len(ecnryptedData))
+func decryptAes128Ecb(encryptedData []byte, key []byte) []byte {
+	cipher, _ := aes.NewCipher(key)
+	decrypted := make([]byte, len(encryptedData))
 	size := 16
-	for bs, be := 0, size; bs < len(ecnryptedData); bs, be = bs+size, be+size {
-		cipher.Decrypt(decrypted[bs:be], ecnryptedData[bs:be])
+	for bs, be := 0, size; bs < len(encryptedData); bs, be = bs+size, be+size {
+		cipher.Decrypt(decrypted[bs:be], encryptedData[bs:be])
 	}
 	return decrypted
 }
--- pkg/files/secret_test.go
+++ pkg/files/secret_test.go
@@ -1,11 +1,16 @@
 package files
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_something(t *testing.T) {
-	Decrypt(
+	secret, err := Decrypt(
 		"../../test/resources/master.key",
 		"../../test/resources/hudson.util.Secret")
+	check(err)
+	fmt.Printf("Secret:%q", secret)
+	assert.True(t, true)
 }
